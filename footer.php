24c5426ae93ebeb0456372fdcf62c87cf1eccbb4
g4zerock@gmail.com <g4zerock@gmail.com>
2024-11-03 18:46:26 +0700

    tes

--- footer.php
+++ footer.php
@@ -3,5 +3,5 @@
     <div class="float-right d-none d-sm-inline">
         Versi 1.0
     </div>
-    <strong>&copy; UTS DWI SANTOSO.</strong> Sistem Manajemen Karyawan | All rights reserved.
+    <strong>&copy; UTS PEMROGRAMAN WEB I DWI SANTOSA.</strong> Sistem Manajemen Karyawan | All rights reserved.
 </footer>
